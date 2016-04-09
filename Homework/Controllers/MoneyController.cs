5ab583db81e275ca6e56a69e330044a1a61155d7
liwen <liwenproject@gmail.com>
2016-04-09 22:06:48 +0800

    Add MoneyList

---
+++ Homework/Controllers/MoneyController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Homework.Models;
+
+namespace Homework.Controllers
+{
+    public class MoneyController : Controller
+    {
+        // GET: Money
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MoneyList()
+        {
+            var model = new List<MoneyListViewModels>();          
+            model.Add(new MoneyListViewModels { Category = "支出", Amount = 100, BillingDate = Convert.ToDateTime("2016/04/01"), Memo = "買蘋果" });
+            model.Add(new MoneyListViewModels { Category = "收入", Amount = 50000, BillingDate = Convert.ToDateTime("2016/04/05"), Memo = "薪資" });
+            model.Add(new MoneyListViewModels { Category = "支出", Amount = 1200, BillingDate = Convert.ToDateTime("2016/04/01"), Memo = "通訊費" });
+            return View(model);
+        }
+    }
+}
---
+++ Homework/Models/MoneyListViewModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Homework.Models
+{
+    public class MoneyListViewModels
+    {
+        public string Category { get; set; }
+        public double Amount { get; set; }
+        public DateTime BillingDate { get; set; }
+        public string Memo { get; set; }
+    }
+}
