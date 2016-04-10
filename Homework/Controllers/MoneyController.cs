e35c15a4579d613bc498bbf970e2fcc625de023d
liwen <liwenproject@gmail.com>
2016-04-10 12:10:54 +0800

    修改下拉是選單樣式

--- Homework/Controllers/MoneyController.cs
+++ Homework/Controllers/MoneyController.cs
@@ -18,10 +18,12 @@ namespace Homework.Controllers
 
         public ActionResult MoneyList()
         {
+            //先做靜態假資料
             var model = new List<MoneyListViewModels>();          
-            model.Add(new MoneyListViewModels { Category = "支出", Amount = 100, BillingDate = Convert.ToDateTime("2016/04/01"), Memo = "買蘋果" });
+            model.Add(new MoneyListViewModels { Category = "支出", Amount = 100, BillingDate = Convert.ToDateTime("2016/04/01"), Memo = "買水果" });
             model.Add(new MoneyListViewModels { Category = "收入", Amount = 50000, BillingDate = Convert.ToDateTime("2016/04/05"), Memo = "薪資" });
             model.Add(new MoneyListViewModels { Category = "支出", Amount = 1200, BillingDate = Convert.ToDateTime("2016/04/06"), Memo = "通訊費" });
+
             return View(model);
         }
     }
