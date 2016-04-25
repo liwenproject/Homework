02d5e8a6f1780a0544e1188f768dafe498045312
liwen <liwenproject@gmail.com>
2016-04-25 23:23:25 +0800

    使用RazorHelper，列表加入顏色變換 ▪ 類型的「支出」字樣顯現為紅色 ▪ 類型的「收入」字樣顯現為藍色

---
+++ Homework/App_Code/MyDictionary.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Drawing;
+using System.Web.Mvc;
+
+namespace Homework.App_Code
+{
+    public class DictInfo
+    {
+        public string Desc { get; set; }
+        public Color ForeColor { get; set; }
+        public Color BackColor { get; set; }
+    }
+
+    public class MyDictionary
+    {
+        public static Dictionary<int, string> Category = new Dictionary<int, string>()
+        {
+            {0, "收入" },
+            {1, "支出" }
+        };
+        public static Dictionary<int, DictInfo> CategoryInfo = new Dictionary<int, DictInfo>()
+        {
+            {0, new DictInfo { Desc=Category[0] , ForeColor=Color.Blue, BackColor=Color.Transparent} },
+            {1, new DictInfo { Desc=Category[1] , ForeColor=Color.Red, BackColor=Color.Transparent} }
+        };
+    }
+}
